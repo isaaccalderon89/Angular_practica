6cd6c346e729e2da1462b60de3859abf52ccd5ac
isaac.calderon <isaaccalderon89@gmail.com>
2022-11-04 19:07:10 +0100

    Primer commit

---
+++ 01_trasteando/prueba.ts
@@ -0,0 +1,6 @@
+let nombre = "tony"//Estamos creando el tipo mediante inferencia
+let apellidos : string = "Stark"//declaramos el tipo
+
+//nombre = 5; ERROR a un tipo cadena no podemos ponerle un valor numérico
+
+console.log(nombre,apellidos)
